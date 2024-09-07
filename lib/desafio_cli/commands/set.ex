7155cc86a16f239897cc033f863d4eb6a35701ab
vrtineu <vininro@gmail.com>
2024-09-07 01:32:05 -0300

    refactor set command to validate and handle invalid values

--- lib/desafio_cli/commands/set.ex
+++ lib/desafio_cli/commands/set.ex
@@ -5,17 +5,31 @@ defmodule DesafioCli.Commands.Set do
 
   alias DesafioCli.Db
 
+  @not_allowed_values ~w(NIL)
+
   def execute({key, value}) do
-    case Db.get(key) do
-      nil ->
-        Db.set(key, value)
+    with true <- is_valid_value?(value) do
+      case Db.get(key) do
+        nil ->
+          Db.set(key, value)
 
-        {:ok, "FALSE #{value}"}
+          {:ok, "FALSE #{value}"}
 
-      _ ->
-        Db.set(key, value)
+        _old_value ->
+          Db.set(key, value)
 
-        {:ok, "TRUE #{value}"}
+          {:ok, "TRUE #{value}"}
+      end
+    else
+      false -> {:error, "ERR \"Valor inválido\""}
     end
   end
+
+  defp is_valid_value?(value) when is_bitstring(value) do
+    not Enum.member?(@not_allowed_values, String.upcase(value))
+  end
+
+  defp is_valid_value?(value) when is_boolean(value), do: true
+  defp is_valid_value?(value) when is_number(value), do: true
+  defp is_valid_value?(_), do: false
 end
